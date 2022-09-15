728cd8b2b3ef72d8384c63c8e2201f3783240c72
Aman Gupta <gaman100071@gmail.com>
2022-09-16 02:08:44 +0530

    changed title

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tailor',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Tailor Home Page'),
     );
   }
 }
